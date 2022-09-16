37a94d9c62986306aa8f3958655e3a50e5f29462
Maros Kukan <maros.kukan@me.com>
2022-09-16 14:21:19 +0200

    Update redirection to /tmp/install.log

---
+++ scripts/06-lvm-hooks.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+set -e
+
+# Description: Arch Linux Initial Installation Script
+# Author: Maros Kukan
+# Usage: curl -s https://raw.githubusercontent.com/maroskukan/workspace-arch/main/scripts/06-lvm-hooks.sh | bash
+
+
+echo "Backing up /etc/mkinitcpio.conf..."
+arch-chroot /mnt cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak &>>/tmp/install.log \
+  && echo -e "\e[32m[OK]\e[0m  Backup created." \
+  || echo -e "\e[31m[NOK]\e[0m Failed to create backup."
+
+echo "Adding lvm2 to initramfs HOOKS..."
+arch-chroot /mnt sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf &>>/tmp/install.log \
+  && echo -e "\e[32m[OK]\e[0m  lvm2 added to HOOKS list." \
+  || echo -e "\e[31m[NOK]\e[0m Failed to add lvm2 to HOOKS list."
+
+echo "Creating initial ramdisk for linux kernel..."
+arch-chroot /mnt mkinitcpio -p linux &>>/tmp/install.log \
+  && echo -e "\e[32m[OK]\e[0m  Ramdisk created." \
+  || echo -e "\e[31m[NOK]\e[0m Failed to ramdisk."
--- scripts/06-lvm_hooks.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-arch-chroot /mnt cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
-arch-chroot /mnt sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf
-
-arch-chroot /mnt mkinitcpio -p linux
