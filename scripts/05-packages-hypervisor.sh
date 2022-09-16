33b854ce08e723e0cfba4c80dc4de521d9a357f2
Maros Kukan <maros.kukan@me.com>
2022-09-16 14:15:17 +0200

    Add support packages for hypervisor

--- scripts/05-packages-hypervisor.sh
+++ scripts/05-packages-hypervisor.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/bash
+set -e
+
+# Description: Arch Linux Initial Installation Script
+# Author: Maros Kukan
+# Usage: curl -s https://raw.githubusercontent.com/maroskukan/workspace-arch/main/scripts/05-packages-hypervisor.sh | bash
+
 
 # Retrieve Host Hypervisor
 declare -xr HYPERVISOR=$(virt-what)
 
 # Install Hypervisor specific packages
 case $HYPERVISOR in
-
-  hyperv)
-    echo -n "Installing packages for Hyper-V support..."
-    arch-chroot /mnt pacman -S --noconfirm hyperv
-    arch-chroot /mnt systemctl enable {hv_fcopy_daemon,hv_kvp_daemon,hv_vss_daemon}
-    ;;
-  virtualbox)
-    echo -n "Installing packages for Virtualbox support..."
-    arch-chroot /mnt pacman -S --noconfirm virtualbox-guest-utils
-    ;;
-  *)
-    echo -n "Skipping installation hypervisor specific packages..."
-  ;;
+    hyperv)
+        echo "Installing packages for Hyper-V support..."
+        arch-chroot /mnt pacman -S --noconfirm hyperv &>>/tmp/install.log \
+            && echo -e "\e[32m[OK]\e[0m  Packages installed." \
+            || echo -e "\e[31m[NOK]\e[0m Failed to install packages."
+        echo "Enabling services for Hyper-V support..."
+        arch-chroot /mnt systemctl enable {hv_fcopy_daemon,hv_kvp_daemon,hv_vss_daemon} &>>/tmp/install.log \
+            && echo -e "\e[32m[OK]\e[0m  Services enabled." \
+            || echo -e "\e[31m[NOK]\e[0m Failed to enable services."
+        ;;
+    virtualbox)
+        echo "Installing packages for Virtualbox support..."
+        arch-chroot /mnt pacman -S --noconfirm virtualbox-guest-utils &>>/tmp/install.log \
+            && echo -e "\e[32m[OK]\e[0m  Packages installed." \
+            || echo -e "\e[31m[NOK]\e[0m Failed to install packages."
+        ;;
+    *)
+        echo "\e[33m[INFO]\e[0m No hypervisor detected skipping, specific packages..."
+        ;;
 esac
