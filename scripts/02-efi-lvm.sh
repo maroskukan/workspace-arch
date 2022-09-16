96f43f4355207b67716ab5ecc1d9babdef7705a4
Maros Kukan <maros.kukan@me.com>
2022-09-16 13:44:21 +0200

    Indentation and filesystem settings

--- scripts/02-efi-lvm.sh
+++ scripts/02-efi-lvm.sh
@@ -32,80 +32,80 @@ ls /sys/firmware/efi/efivars &>/dev/null \
 #     esac
 # done
 wipefs --all --force /dev/sda &>>/tmp/install.log \
-&& echo -e "\e[32m[OK]\e[0m  Filesystem wiped." \
-|| echo -e "\e[31m[NOK]\e[0m Failed wipe filelsystem."
+    && echo -e "\e[32m[OK]\e[0m  Filesystem wiped." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to wipe filelsystem."
 
 # Create new empty GPT partition table
 echo "Creating GPT label at /dev/sda..."
 parted -s /dev/sda mklabel gpt &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  GPT label added." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to add GPT label."
+    && echo -e "\e[32m[OK]\e[0m  GPT label added." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to add GPT label."
 
 
 # Create new primary partition
 echo "Creating EFI partition..."
 parted -a optimal /dev/sda --script mkpart primary fat32 1MiB 501MiB &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  EFI partition created." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to create EFI partition."
+    && echo -e "\e[32m[OK]\e[0m  EFI partition created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create EFI partition."
 
 
 echo "Creating root partition..."
 parted -a optimal /dev/sda --script mkpart primary ext4 501MiB 100% &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  Root partition created." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to create Root partition."
+    && echo -e "\e[32m[OK]\e[0m  Root partition created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create Root partition."
 
 
 # Update partition type
 echo "Setting EFI partition type to EFI..."
 parted -a optimal /dev/sda --script set 1 esp on &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  EFI partition type updated." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to set EFI partition type."
+    && echo -e "\e[32m[OK]\e[0m  EFI partition type updated." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to set EFI partition type."
 
 
 echo "Setting root partition type to LVM..."
 parted -a optimal /dev/sda --script set 2 lvm on &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  LVM partition type updated." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to set LVM partition type."
+    && echo -e "\e[32m[OK]\e[0m  LVM partition type updated." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to set LVM partition type."
 
 
 # Create new physical volume
 echo "Creating LVM physical volume..."
 pvcreate --dataalignment 1m /dev/sda2 &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  LVM physical volume created." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to create LVM physical volume."
+    && echo -e "\e[32m[OK]\e[0m  LVM physical volume created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create LVM physical volume."
 
 # Create a new volume group
 echo "Creating LVM volume group..."
 vgcreate volgroup0 /dev/sda2 &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  LVM volume group created." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to create LVM volume group."
+    && echo -e "\e[32m[OK]\e[0m  LVM volume group created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create LVM volume group."
 
 # Create logical volume for root
 echo "Creating logical volume for root..."
-lvcreate -L 16G volgroup0 -n lv_root &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  LVM logical volume created." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to create LVM logical volume."
+lvcreate --yes -L 16G volgroup0 -n lv_root &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  LVM logical volume created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create LVM logical volume."
 
 # Create logical volume for home
 echo "Creating logical volume for home..."
-lvcreate -l 100%FREE volgroup0 -n lv_home &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  LVM logical volume created." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to create LVM logical volume."
+lvcreate --yes -l 100%FREE volgroup0 -n lv_home &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  LVM logical volume created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create LVM logical volume."
 
 # Load kernel module
 echo "Loading device mapper kernel module..."
 modprobe dm_mod &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  Kernel module loaded." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to load kernel module."
+    && echo -e "\e[32m[OK]\e[0m  Kernel module loaded." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to load kernel module."
 
 # Scan for volume groups
 echo "Scaning for logical volumes..."
 vgscan &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  Volume scan successfull." \
-  || echo -e "\e[31m[NOK]\e[0m Failed scan new volumes."
+    && echo -e "\e[32m[OK]\e[0m  Volume scan successfull." \
+    || echo -e "\e[31m[NOK]\e[0m Failed scan new volumes."
 
 # Activate
 echo "Activating new volume group..."
 vgchange -ay &>>/tmp/install.log \
-  && echo -e "\e[32m[OK]\e[0m  Volume group activated." \
-  || echo -e "\e[31m[NOK]\e[0m Failed to activate volume group."
+    && echo -e "\e[32m[OK]\e[0m  Volume group activated." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to activate volume group."
--- scripts/03-filesystem.sh
+++ scripts/03-filesystem.sh
@@ -1,20 +1,48 @@
 #!/usr/bin/bash
+set -e
+
+# Description: Arch Linux Initial Installation Script
+# Author: Maros Kukan
+# Usage: curl -s https://raw.githubusercontent.com/maroskukan/workspace-arch/main/scripts/03-filesystem.sh | bash
+
 
 # Create Fat32 for EFI parition
-mkfs.fat -F32 /dev/sda1
+echo "Creating filesystem for EFI partition..."
+mkfs.fat -F32 /dev/sda1 &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  Filesystem created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create filesystem."
 
 # Create Ext4 for root
-mkfs.ext4 /dev/volgroup0/lv_root
+echo "Creating filesystem for root volume..."
+mkfs.ext4 /dev/volgroup0/lv_root &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  Filesystem created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create filesystem."
 
 # Create Ext4 for home
-mkfs.ext4 /dev/volgroup0/lv_home
+echo "Creating filesystem for home volume..."
+mkfs.ext4 /dev/volgroup0/lv_home &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  Filesystem created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create filesystem."
 
 # Mount root fs
-mount /dev/volgroup0/lv_root /mnt
+echo "Mounting root volume..."
+mount /dev/volgroup0/lv_root /mnt &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  Volume mounted." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to mount volume."
 
 # Mount /home 
-mount --mkdir /dev/volgroup0/lv_home /mnt/home
+echo "Mounting home volume..."
+mount --mkdir /dev/volgroup0/lv_home /mnt/home &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  Volume mounted." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to mount volume."
 
 # Generate fstab
-mkdir /mnt/etc
-genfstab -U -p /mnt >> /mnt/etc/fstab
+echo "Creating /etc on target..."
+mkdir /mnt/etc &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  Folder created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create folder."
+
+echo "Generating filelsystem table on target..."
+genfstab -U -p /mnt >> /mnt/etc/fstab &>>/tmp/install.log \
+    && echo -e "\e[32m[OK]\e[0m  Filesystem table created." \
+    || echo -e "\e[31m[NOK]\e[0m Failed to create filesystem table."
