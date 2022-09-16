eb0b323c3e04be061410605322ee222ba4599c1d
Maros Kukan <maros.kukan@me.com>
2022-09-16 11:16:42 +0200

    Donate pacman some time

--- scripts/04-packages-shared.sh
+++ scripts/04-packages-shared.sh
@@ -11,5 +11,8 @@ pacstrap /mnt base linux linux-headers
   && echo "[OK]  Packstrap was completed." \
   || echo "[NOK] Failed to pacstrap the base."
 
+# Sometimes pacman fails to unlock db in time
+sleep 10
+
 arch-chroot /mnt pacman -S --noconfirm vim git base-devel openssh networkmanager dialog lvm2
 arch-chroot /mnt systemctl enable {sshd,NetworkManager}
