9b06ccc34cd8d082b8a31286ce7160239b032aeb
Maros Kukan <maros.kukan@me.com>
2022-09-16 12:09:45 +0200

    Add reponse code handling

--- scripts/01-init.sh
+++ scripts/01-init.sh
@@ -3,17 +3,37 @@
 # Description: Arch Linux Initial Installation Script
 # Author: Maros Kukan
 
+
 # Enable NTP time synchronization
-timedatectl set-ntp true
+echo "Enabling NTP time synchronization..."
+timedatectl set-ntp true &>/dev/null \
+  && echo -e "\e[32m[OK]\e[0m  NTP timesync was enabled." \
+  || echo -e "\e[31m[NOK]\e[0m Failed enable NTP timesync."
+
 
 # Update mirrors list
-reflector > /etc/pacman.d/mirrorlist
+echo "Uptating pacman mirror list for live environment..."
+reflector > /etc/pacman.d/mirrorlist &>/dev/null \
+  && echo "\e[32m[OK]\e[0m  Mirrors list updated." \
+  || echo "\e[31m[NOK]\e[0m Failed to update mirror list."
+
 
 # Enable parallel downloads
-sed -i 's/^#ParallelDownloads =.*/ParallelDownloads = 5/' /etc/pacman.conf
+echo "Setting parallel downloads to 5..."
+sed -i 's/^#ParallelDownloads =.*/ParallelDownloads = 5/' /etc/pacman.conf \
+  && echo "\e[32m[OK]\e[0m  Parallel downloads count set to 5." \
+  || echo "\e[31m[NOK]\e[0m Failed to update parallel downloads count."
+
 
 # Synchronize package databases
-pacman -Sy
+echo "Synchronizing pacman database for live environment..."
+pacman -Sy &>/dev/null \
+  && echo "\e[32m[OK]\e[0m  Pacman database updated." \
+  || echo "\e[31m[NOK]\e[0m Failed to update pacman database."
+
 
 # Install packages in live environment
-pacman -S --noconfirm virt-what
+echo "Installing virt-what package for live environment..."
+pacman -S --noconfirm virt-what &>/dev/null \
+  && echo "\e[32m[OK]\e[0m  Virt-what package installed." \
+  || echo "\e[31m[NOK]\e[0m Failed to install virt-what package."
