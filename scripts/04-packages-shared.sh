8cb521dac9e92e4b5ec8df57a914c7871d306b10
Maros Kukan <maros.kukan@me.com>
2022-09-16 11:18:23 +0200

    Redirect stdout and stderr to /dev/null

--- scripts/04-packages-shared.sh
+++ scripts/04-packages-shared.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 # Update mirror list
-echo "Updating mirror list."
-reflector > /etc/pacman.d/mirrorlist \
+echo "Updating mirror list..."
+reflector > /etc/pacman.d/mirrorlist &>/dev/null \
   && echo "[OK]  Mirrors list updated." \
   || echo "[NOK] Failed to update mirror list."
 
 # Kernel and headers
-pacstrap /mnt base linux linux-headers 
+echo "Packstrapping base and kernel..."
+pacstrap /mnt base linux linux-headers &>/dev/null
   && echo "[OK]  Packstrap was completed." \
   || echo "[NOK] Failed to pacstrap the base."
 
